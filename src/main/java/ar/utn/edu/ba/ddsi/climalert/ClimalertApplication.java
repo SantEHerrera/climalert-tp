c92d44e07048141437c01b10047858c0c6a31eb1
Santiago Herrera <santherrera@frba.utn.edu.ar>
2026-07-05 14:27:04 -0300

    agrego @Scheduling

--- src/main/java/ar/utn/edu/ba/ddsi/climalert/ClimalertApplication.java
+++ src/main/java/ar/utn/edu/ba/ddsi/climalert/ClimalertApplication.java
@@ -2,8 +2,10 @@ package ar.utn.edu.ba.ddsi.climalert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ClimalertApplication {
 
 	public static void main(String[] args) {
